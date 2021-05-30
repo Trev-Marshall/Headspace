efc5aeea5ec029fe8d3aa5d9871dbec5795cfc7a
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-05-30 19:41:35 -0400

    Styling constants and changes

---
+++ src/Design/Constants.js
@@ -0,0 +1,8 @@
+export const COLORS1 = {
+  main: '#3A0CA3',
+  secondary_main: '#3F37C9',
+  main_accent: '#B5179E',
+  secondary_accent: '#F72585',
+  bright_color: '#4CC9F0',
+  off_white: '#EDDBB8'
+}
