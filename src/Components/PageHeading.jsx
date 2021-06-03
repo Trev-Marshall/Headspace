98e0b25a00275139e6483ed6e38e16b2b3cc5e6c
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-06-02 20:49:45 -0400

    Badass border on reflections and goals page headers

--- src/Components/PageHeading.jsx
+++ src/Components/PageHeading.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { COLORS1 } from '../Design/Constants'
 
 function PageHeading({value}) {
   return (
@@ -12,6 +13,29 @@ function PageHeading({value}) {
 export default PageHeading
 
 const H1 = styled.h1`
+  position: relative;
   opacity: .8;
   font-size: 4em;
+  &:before {
+    content: '';
+    position: absolute;
+    top: -20px;
+    left: -30px;
+    width: 20px;
+    height: 20px;
+    background: transparent;
+    border-top: 6px solid ${COLORS1.bright_color};
+    border-left: 6px solid ${COLORS1.bright_color};
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    right: -30px;
+    bottom: -20px;
+    width: 20px;
+    height: 20px;
+    background: transparent;
+    border-bottom: 6px solid ${COLORS1.bright_color};
+    border-right: 6px solid ${COLORS1.bright_color};
+  }
 `
