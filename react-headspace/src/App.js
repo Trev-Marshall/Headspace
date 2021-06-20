f6c3c57593f84adb2325082e6d1b61823a94b4f7
Trev-Marshall <trevor.h.marshall@icloud.com>
2021-06-20 16:50:55 -0400

    Ported all pages and styling from the original react front end

--- react-headspace/src/App.js
+++ react-headspace/src/App.js
@@ -1,38 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
+import NavBar from './Components/NavBar'
+import TodoList from './Components/TodoList'
+import Footer from './Components/Footer'
+import styled from 'styled-components'
+import Signin from './Components/Signin';
+import { COLORS1 } from './Design/Constants'
+import Profile from './Components/Profile';
+import Goals from './Components/Goals'
+import Reflections from './Components/Reflections'
 
 function App() {
-  const [tasks, setTasks] = useState({
-  })
-
-  useEffect(() => {
-    let data = {};
-    axios.get('http://127.0.0.1:8000/wel/')
-      .then(res => {
-        data = res.data;
-        setTasks({
-          details: data
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, [])
-
-  console.log(tasks.details);
-
   return (
-    <div>
-      <ul>
-        {tasks.details &&
-          tasks.details.map((details, id) => (
-            <p key={id}>{details.task}</p>
-          )
-          )}
-      </ul>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/goals">
+          <Goals />
+        </Route>
+        <Route path="/reflections">
+          <Reflections />
+        </Route>
+        <Route path="/">
+          <NavBar />
+          <Container>
+            <TodoList />
+            <Footer />
+          </Container>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  height: calc(100vh - 60px);
+  background-color: ${COLORS1.secondary_main};
+`
