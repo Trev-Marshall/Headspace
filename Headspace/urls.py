4c98cb5c3703dbe6003fa05e5f4a2cd38b12412e
Trev-Marshall <trevor.h.marshall@icloud.com>
2021-06-26 06:48:36 -0400

    Finished backend

--- Headspace/urls.py
+++ Headspace/urls.py
@@ -22,5 +22,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wel/', ReactView.as_view(), name='something'),
-    path('token-auth/', obtain_jwt_token)
+    path('token-auth/', obtain_jwt_token),
+    path('core/', include('core.urls'))
 ]
