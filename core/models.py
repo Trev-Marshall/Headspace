6d332b2976ff3d8118c3d638f939b4aaf989ae95
Hamed Polo <hamedpolo62@gmail.com>
2021-07-06 20:20:34 -0400

    added goals and reflection serializers

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    task = models.CharField(max_length=50)
+    task = models.CharField(max_length=100)
     details = models.TextField(max_length=600)
     completed = models.BooleanField(default=True)
     dateCreated = models.DateField(auto_now_add=True)
@@ -12,13 +12,14 @@ class Task(models.Model):
 
 
 class Relfection(models.Model):
-    reflection = models.CharField(max_length=75)
+    reflection = models.CharField(max_length=150)
     dateCreated = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
 
 class Goals(models.Model):
-    goal = models.CharField(max_length=50)
+    goal = models.CharField(max_length=100)
     completeBy = DateField()
+    completed = models.BooleanField(default=True)
     dateCreated = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
