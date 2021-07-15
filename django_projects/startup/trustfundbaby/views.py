e72c5ebe4bced0bf02a2e9d8d121c34b557409f8
milner-chen <milnerchen0@gmail.com>
2021-07-15 10:49:42 -0400

    login

--- django_projects/startup/trustfundbaby/views.py
+++ django_projects/startup/trustfundbaby/views.py
@@ -5,9 +5,22 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import SignUpForm
 def home(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+    #pass
 
-    return render(request, 'home.html')
+    #return render(request, 'home.html')
+    
 
 def works(request):  
 
