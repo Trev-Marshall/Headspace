26c1332984c8900e89b83528dc818387c8981d39
SYLVIE <sylvierui1@gmail.com>
2021-07-15 11:15:39 -0400

    blog

--- django_projects/startup/trustfundbaby/urls.py
+++ django_projects/startup/trustfundbaby/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('edu/', views.edu, name='edu'),
     path('faq/', views.faq, name='faq'),
     path('achv/', views.achv, name='achv'),
-
+    path('state/', views.state, name='state'),
+    path('tax/', views.tax, name='tax'),
+    path('investment/', views.investment, name='investment'),
+    path('strats/', views.strats, name='strats'),
+    path('extra/', views.strats, name='extra'),
     path('signup/', views.signup, name='signup')
 ]
